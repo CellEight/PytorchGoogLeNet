b13bef89c87042c4d03be905bacae2b67da2cf33
Lewis Patten <lp921@york.ac.uk>
2021-03-02 21:50:43 +0000

    model 95% done

--- model.py
+++ model.py
@@ -2,34 +2,139 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class AlexNet(nn.Module):
-    """ A implementation of the AlexNet architecture from the paper 'ImageNet 
-        Classification with Deep Convolutional Neural Networks' """
-    def __init__(self):
+    """ A implementation of the GoogLeNet architecture from the paper '' """
+    def __init__(self, n_class):
         super().__init__()
-        # The paper specifies that this layer should be stride 4 however this does not work with 
-        # the input dimensions, I work under the assumption that the definition of stride used 
-        # in the Alexnet paper and the pytorch library differ.
-        self.conv1 = nn.Conv2d(3, 96, kernel_size=11, stride=2, padding=5, padding_mode='reflect') 
-        self.conv2 = nn.Conv2d(96, 256, kernel_size=5, stride=1, padding=2, padding_mode='reflect')
-        self.conv3 = nn.Conv2d(256, 384, kernel_size=3, stride=1, padding=1, padding_mode='reflect')
-        self.conv4 = nn.Conv2d(384, 384, kernel_size=3, stride=1, padding=1, padding_mode='reflect')
-        self.conv5 = nn.Conv2d(384, 256, kernel_size=3, stride=1, padding=1, padding_mode='reflect')
-        self.fc6 = nn.Linear(43264,4096)
-        self.fc7 = nn.Linear(4096,4096)
-        self.fc8 = nn.Linear(4096,1000)
-        self.pool = nn.MaxPool2d(3,2)
-        self.drop = nn.Dropout(0.5)
+        self.train = True
+        
+        # Layers 1 to 4
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, padding_mode='reflect') 
+        self.redu1 = nn.Conv2d(64,64,kernel_size=1, stride=1)
+        self.conv2 = nn.Conv2d(64, 192, kernel_size=3, stride=1, padding=1, padding_mode='reflect')
+        
+        # Layers 5 to 8
+        self.incept1 = Inception(192,64,(96,128),(16,32),32)
+        self.incept2 = Inception(256,128,(128,192),(32,96),64)
+        self.incept3 = Inception(480,192,(96,208),(16,48),64)
+        
+        # Optional Classifcation Path 1
+        self.redu2 = nn.Conv2d(512,128, kernel_size=1,stride=1)
+        self.fc1(128*4*4,1024)
+        self.fc2(1024, n_class)
+        
+        # Layers 9 to 11
+        self.incept4 = Inception(512,160,(112,224),(24,64),64)
+        self.incept5 = Inception(512,128,(128,256),(24,64),64)
+        self.incept6 = Inception(512,112,(144,288),(32,64)64)
+        
+        # Optional Classifcation Path 2
+        self.redu3 = nn.Conv2d(528,128, kernel_size=1,stride=1)
+        self.fc3(128*4*4,1024)
+        self.fc4(1024, n_class) 
+        
+        # Layers 12 to 15
+        self.incept7 = Inception(528,256,(160,320),(32,128),128)
+        self.incept8 = Inception(832,256,(160,320),(32,128),128)
+        self.incept9 = Inception(832,384,(192,384),(48,128),128)
+        
+        # Final Classifcation Path
+        self.fc5(1024,n_class)
+        
+        # Unparamartried (spelling?) layers
+        self.avg_pool1 = nn.AvgPool2d(5, stride=3)
+        self.avg_pool2 = nn.AvgPool2d(7, stride=1)
+        self.max_pool = nn.MaxPool2d(3,stride=2)
+        self.drop1 = nn.Dropout(0.7)
+        self.drop2 = nn.Dropout(0.4)
         self.lrn = nn.LocalResponseNorm(size=5,alpha=10e-4,beta=0.75,k=2.0)
+        
 
     def forward(self, x):
-        x = self.lrn(self.pool(F.relu(self.conv1(x))))
-        x = self.lrn(self.pool(F.relu(self.conv2(x))))
-        x = F.relu(self.conv3(x))
-        x = F.relu(self.conv4(x))
-        x = self.pool(F.relu(self.conv5(x)))
-        x = x.view(-1,43264)
-        x = F.relu(self.drop(self.fc6(x)))
-        x = F.relu(self.drop(self.fc7(x)))
-        x = self.fc8(x)
-        return x
+        # Layers 1 to 4
+        x = F.relu(self.conv1(x))
+        x = self.lrn(self.pool(x))
+        x = F.relu(self.redu1(x))
+        x = F.relu(self.conv2(x))
+        x = self.max_pool(self.lrn(x))
 
+        # Layers 5 to 8
+        x = self.incept1(x)
+        x = self.incept2(x)
+        x = self.max_pool(x)
+        x = self.incept3(x)
+        
+        if train:
+            # Optional Classifcation Path 1
+            x1 = self.avg_pool1(x)
+            x1 = F.relu(self.redu2(x1))
+            x1 = x1.view(-1,128*4*4)
+            x1 = F.relu(self.fc1(x1))
+            x1 = F.relu(self.fc2(x1))
+            # softmax?
+        
+        # Layers 9 to 11
+        x = self.incept4(x)
+        x = self.incept5(x)
+        x = self.incept6(x)
+        
+        if train:
+            # Optional Classifcation Path 2
+            x2 = self.avg_pool1(x)
+            x2 = F.relu(self.redu2(x2))
+            x2 = x1.view(-1,128*4*4)
+            x2 = F.relu(self.fc1(x2))
+            x2 = F.relu(self.fc2(x2))
+            # softmax?
+
+        # Layers 12 to 15
+        x = self.incept7(x)
+        x = self.max_pool(x)
+        x = self.incept8(x)
+        x = self.incept9(x)
+        
+        # Final Classifcation Path
+        x = self.avg_pool2(x)
+        x = torch.view(-1,1024)
+        x = self.fc3(x)
+        # softmax?
+        
+        if train:
+            return x1, x2, x3
+        else:
+            return x
+
+class Inception(nn.Module):
+    """ An implementation of the Inception blocks desired in the GoogLeNet Paper """
+    def __init__(self, dim_in, dim_1, dims_3, dims_5, dim_pool):
+        super().__init__()
+        # 1x1 Convolution Path
+        self.redu1 = nn.Conv2d(dim_in, dim_1, kernel_size=1, stride=1) 
+        # 3x3 Convolution Path
+        self.redu2 = nn.Conv2d(dim_in,dims_3[0], kernel_size=1, stride=1)
+        self.conv1 = nn.Conv2d(dims_3[0], dims_3[1], kernel_size=3, stride=1, padding=1, padding_mode='reflect')
+        # 5x5 Convolution Path
+        self.redu3 = nn.Conv2d(dim_in, dims_5[1], kernel_size=1, stride=1)
+        self.conv2 = nn.Conv2d(dims_5[0]], dims_5[1], kernel_size=5, stride=1, padding=1, padding_mode='reflect')
+        # Max-Pool Path
+        self.pool  = nn.MaxPool2d(3,2)
+        self.redu4 = nn.Conv2d(dim_in, dim_pool, kernel_size=1,stride=1)
+
+    def forward(self,x):
+        # 1x1 Convolution Path
+        x1 = F.relu(self.redu1(x))
+        
+        # 3x3 Convolution Path
+        x2 = F.relu(self.redu2(x))
+        x2 = F.relu(self.conv1(x))
+        
+        # 5x5 Convolution Path
+        x3 = F.relu(self.redu3(x))
+        x3 = F.relu(self.conv2(x))
+        
+        # Max-Pool Path
+        x4 = self.pool(x)
+        x4 = F.relu(self.redu4(x))
+
+        #  Concatenate the resulting tensors
+        
+        return x
--- train.py
+++ train.py
@@ -16,8 +16,10 @@ def train(model, train_dl, test_dl, opt, loss_func, epochs):
     """ train model using using provided datasets, optimizer and loss function """
     train_loss = [0 for i in range(epochs)]
     test_loss = [0 for i in range(epochs)]
+    
     for epoch in range(epochs):
         model.train()
+        
         for xb, yb in train_dl:
             xb, yb = xb.to(device), yb.to(device)
             loss = loss_func(model(xb), yb)
@@ -25,11 +27,13 @@ def train(model, train_dl, test_dl, opt, loss_func, epochs):
             loss.backward()
             opt.step()
             opt.zero_grad()
+        
         with torch.no_grad():
             losses, nums = zip(*[(loss_func(model(xb.to(device)),yb.to(device)).item(),len(xb.to(device))) for xb, yb in test_dl])
             test_loss[epoch] = np.sum(np.multiply(losses, nums)) / np.sum(nums)
             correct = 0
             total = 0
+            
             for data in test_dl:
                 images, labels = data
                 images, labels = images.to(device), labels.to(device)
@@ -37,23 +41,29 @@ def train(model, train_dl, test_dl, opt, loss_func, epochs):
                 _, predicted = torch.max(outputs.data, 1)
                 total += labels.size(0)
                 correct += (predicted == labels).sum().item()
+        
         print(f'Epoch: {epoch+1}/{epochs}, Train Loss: {train_loss[epoch]}, Test Loss {test_loss[epoch]}, Accuracy: {100*correct/total}')
+    
     return train_loss, test_loss
 
 if __name__ == "__main__":
     # Define transforms
     transform = transforms.Compose([torchvision.transforms.ToTensor(),transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+    
     # Load training data
     dataset = datasets.ImageFolder('./data', transform=transform) 
     test_data, train_data = random_split(dataset,(4396,1099), generator=torch.Generator().manual_seed(42))
     train_dl = torch.utils.data.DataLoader(train_data, batch_size=64, shuffle=True, num_workers=4)
     test_dl = torch.utils.data.DataLoader(test_data, batch_size=64, shuffle=True, num_workers=4)
+    
     # Train Model
+    # Some modification will be required here due to the somewhat strange method of training for googlenet
     epochs = 25
-    model = AlexNet().to(device)
-    loss_func = nn.CrossEntropyLoss()
+    model = GoogLeNet().to(device)
+    loss_func = nn.CrossEntropyLoss() # Maybe be able to address all the issues by either writing a custom loss function of just taking a weighted sum
     opt = optim.Adam(model.parameters(), lr=0.0001)
     train_loss, test_loss = train(model, train_dl, test_dl, opt, loss_func, epochs)
+    
     # Save Model to pkl file
     f = open(f'model.pkl','wb')
     pickle.dump(model,f)
